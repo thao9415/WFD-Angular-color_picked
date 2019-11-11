789a3c8e04e4d61fc85a7898c049253bbf8f69da
thao <thao.9415@gmail.com>
2019-11-11 17:32:45 +0700

    first

---
+++ src/app/color/color.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+
+  background = '#000000';
+  onChane(value) {
+    this.background = value;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
